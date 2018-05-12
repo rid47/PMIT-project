fc7479c4ee62ea2ead0583c7235a84df7ff8a920
rid47 <ridwanmizan@gmail.com>
2018-05-12 21:10:27 +0600

    Reading button press as egg presence

--- smart_egg_tray/smart_egg_tray.ino
+++ smart_egg_tray/smart_egg_tray.ino
@@ -1,9 +1,45 @@
+#define eggPin1 D0
+
+
+//------------------------REQUIRED VARIABLES--------------------------------------//
+
+
+int eggPresence1=0;
+_Bool flag1=false;
+
+
+//-------------------------SET UP FUNCTION----------------------------------------//
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  pinMode(eggPin1,INPUT);
 
 }
 
+
+//-------------------------MAIN LOOP----------------------------------------------//
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  eggPresence1=digitalRead(eggPin1);
+  Serial.println(eggPresence1);
+  delay(1000);
+
+  
+  
+  if(eggPresence1==1 && flag1==true)
+  
+  {
+  Serial.println("Pot 1 occupied");
+  flag1=false;
+  }
+  
+  else if (eggPresence1==0 && flag1==false)    
+  {
+  Serial.println("Pot 1 is empty");
+  flag1=true;
+  }
 
 }
